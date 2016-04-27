b33ca1d142737e4726ad10bd179dde2319d16a9a
jhon <jhone.cortesg@gmail.com>
2016-04-26 20:36:33 -0500

    3.03 Get location of Preferences

--- app/src/main/java/com/example/jhon/android_curse/ForecastFragment.java
+++ app/src/main/java/com/example/jhon/android_curse/ForecastFragment.java
@@ -1,8 +1,10 @@
 package com.example.jhon.android_curse;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.format.Time;
@@ -61,8 +63,9 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            String zip_code = "94043";
-            weatherTask.execute(zip_code);
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
