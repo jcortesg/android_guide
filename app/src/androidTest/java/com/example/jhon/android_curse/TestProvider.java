af976eefeaf30c6c9cd0ec61e27cf26dbf588cba
jhon <jhone.cortesg@gmail.com>
2016-04-27 21:49:44 -0500

    5.04 Register the ContentProvider

--- app/src/androidTest/java/com/example/jhon/android_curse/TestProvider.java
+++ app/src/androidTest/java/com/example/jhon/android_curse/TestProvider.java
@@ -1,6 +1,9 @@
 package com.example.jhon.android_curse;
 
+import android.content.ComponentName;
 import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.content.pm.ProviderInfo;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
@@ -99,5 +102,29 @@ public class TestProvider extends AndroidTestCase {
             returnContentValues[i] = weatherValues;
         }
         return returnContentValues;
+    }
+
+    public void testProviderRegistry() {
+        PackageManager pm = mContext.getPackageManager();
+
+        // We define the component name based on the package name from the context and the
+        // WeatherProvider class.
+        ComponentName componentName = new ComponentName(mContext.getPackageName(),
+                WeatherProvider.class.getName());
+        try {
+            // Fetch the provider info using the component name from the PackageManager
+            // This throws an exception if the provider isn't registered.
+            ProviderInfo providerInfo = pm.getProviderInfo(componentName, 0);
+
+            // Make sure that the registered authority matches the authority from the Contract.
+            assertEquals("Error: WeatherProvider registered with authority: " + providerInfo.authority +
+                            " instead of authority: " + WeatherContract.CONTENT_AUTHORITY,
+                    providerInfo.authority, WeatherContract.CONTENT_AUTHORITY);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            // I guess the provider isn't registered correctly.
+            assertTrue("Error: WeatherProvider not registered at " + mContext.getPackageName(),
+                    false);
+        }
     }
 }
